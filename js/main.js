24b8286ff2bc87c0bd20c7d3686e2b3a67b565c4
Dave Mayo <dave_mayo@harvard.edu>
2018-04-18 19:57:12 -0400

    Testing out collapsing :id routes - really, oughtta check how much of a diff it actually makes, and really really need to do the collapsing UI for nested routes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 $(function (){
+  const method_order = {get: 0, put: 1, post: 2, delete: 3, head: 4};
   const render_tmpl = _.template("<tr><td>${uri}</td><td>${method}</td><td>${description}</td></tr>")
   const renderTable = function (endpoints) {
-    const grouped = _.sortBy(_.toPairs(_.groupBy(endpoints, "uri")));
+    const grouped = _.sortBy(_.toPairs(_.groupBy(endpoints, ep => ep.uri.replace(/\/:id$/, ''))), "[0]");
     const out = _.map(grouped, (pair) => render_tmpl({
       uri: pair[0],
-      method: _.map(pair[1], "method[0]").join(", "),
+      method: _.sortBy(_.uniq(_.flatten(_.map(pair[1], "method"))), x => method_order[x]).join(", "),
       description: _.map(pair[1], "description").join(", ")})).join("\n");
     $('#routes>tbody').html(out);
   };
